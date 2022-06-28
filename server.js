86625560400b18cab6f92c9c7b1ca3f98f337269
AhmadMudassir <ahmadmudassir548@gmail.com>
2022-06-28 19:14:52 +0500

    git added

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(DB, {
     useFindAndModify: false
 }).then( () =>console.log('DB connection successful!'));
 
-const port=3000
-app.listen(3000, () => {
+const port=process.env.PORT || 3000
+app.listen(port, () => {
     console.log(`Server running at port ${port}`);
 })
