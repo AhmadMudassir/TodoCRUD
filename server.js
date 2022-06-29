ecf6f793e9d2406471cc90f6c32c8fa70d53c2af
AhmadMudassir <ahmadmudassir548@gmail.com>
2022-06-29 14:26:27 +0500

    Git added

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ const DB=process.env.DATABASE.replace(
 
 mongoose.connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
 }).then( () =>console.log('DB connection successful!'));
 
 const port=process.env.PORT || 3000
