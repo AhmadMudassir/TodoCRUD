0fb8dcc434299e9081a57246330507c62ec30d88
AhmadMudassir <ahmadmudassir548@gmail.com>
2022-06-29 18:20:25 +0500

    Git added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const todoRouter = require('./routes/todoRouter')
 
 const app = express()
-
+app.use(cors())
 app.use(morgan('dev'))
 
 app.use(express.json())
